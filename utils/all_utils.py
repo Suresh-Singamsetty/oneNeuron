e05d01819b0ef22b7780eb0e0ef9248faea8bd7c
Suresh-Singamsetty <sureshsmart96@gmail.com>
2021-10-03 01:08:47 +0530

    docstring updated

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -8,11 +8,30 @@ import os
 plt.style.use("fivethirtyeight")
 
 def prepare_data(df):
+    """it is used to seperate dependent variables and independent features     
+    
+    Args:
+        df(pd.DataFrame): its the pandas DataFrame
+    
+    Returns:
+        tuple: it returns the tuples of dependent and independent features
+    
+    """
     X = df.drop("y", axis=1)
     y = df["y"]
     return X, y
 
 def save_model(model, filename):
+    """This saves the python model to
+    
+    Args:
+        model(python object): train model to
+        filename (str): path to save the model
+    
+    :param df:its a DataFrame
+    :param filename: its path to save the model
+    :param model: train the model
+    """
     model_dir = "models"
     os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
     filePath = os.path.join(model_dir, filename) # model/filename
